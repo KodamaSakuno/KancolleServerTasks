79639f247f1e6d1bd174a1da2e01d940e43a684c
神樹桜乃 <kodama@sakuno.moe>
2021-05-31 16:10:12 +0800

    Add api_start2 watcher

---
+++ ApiStart2Watcher/Program.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Npgsql;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+var logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+
+await using var connection = new NpgsqlConnection(Environment.GetEnvironmentVariable("DatabaseConn") ?? throw new InvalidOperationException("Missing DatabaseConn"));
+
+using var client = new HttpClient();
+
+var gameUrl = await connection.ExecuteScalarAsync<string>("SELECT value #>> '{}' FROM store WHERE name = 'game_url';");
+
+var token = Regex.Match(gameUrl, @"api_token=(\w{40})").Groups[1].Value!;
+var startTime = long.Parse(Regex.Match(gameUrl, @"api_starttime=(\d+)").Groups[1].Value);
+
+var duration = DateTimeOffset.Now - DateTimeOffset.FromUnixTimeMilliseconds(startTime);
+if (duration >= TimeSpan.FromMinutes(115))
+{
+    logger.Information("Token outdated");
+    return;
+}
+
+using var request = new HttpRequestMessage(HttpMethod.Post, "http://125.6.189.167/kcsapi/api_start2/getData");
+request.Headers.Referrer = new Uri(gameUrl);
+request.Content = new FormUrlEncodedContent(new[] {
+    KeyValuePair.Create("api_token", token),
+    KeyValuePair.Create("api_verno", "1"),
+});
+
+using var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
+using var stream = await response.Content.ReadAsStreamAsync();
+
+await stream.ReadAsync(new byte[7]);
+
+using var reader = new JsonTextReader(new StreamReader(stream));
+var json = await JObject.LoadAsync(reader);
+
+var obj = (JObject)json["api_data"]["api_mst_const"];
+
+json["api_data"]["api_mst_const"] = new JObject(obj.Properties().OrderBy(r => r.Name));
+
+var argument = new { json = json["api_data"].ToString(Formatting.None) };
+var ra = await connection.ExecuteAsync("INSERT INTO api_start2_history VALUES(now(), @json::jsonb, convert_to(@json, 'UTF8')) ON CONFLICT DO NOTHING;", argument);
+if (ra == 0)
+{
+    logger.Information("Not Modified");
+    return;
+}
+
+await connection.ExecuteAsync(@"INSERT INTO current_api_start2 SELECT key, value FROM jsonb_each(@json::jsonb)
+ON CONFLICT (key) DO UPDATE SET value = excluded.value
+WHERE current_api_start2.value != excluded.value;", argument);
+
+logger.Information("Saved");
