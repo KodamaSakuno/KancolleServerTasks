06cd727e4c452fab0f07cb3fd64583100d10eb02
神樹桜乃 <kodama@sakuno.moe>
2021-12-03 00:21:49 +0800

    Use playwright instead

--- DmmLogin/Program.cs
+++ DmmLogin/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Dapper;
+using Microsoft.Playwright;
 using Npgsql;
-using PuppeteerSharp;
-using PuppeteerSharp.Input;
 using Serilog;
 using System;
 
@@ -9,62 +8,65 @@ await using var pg = new NpgsqlConnection(Environment.GetEnvironmentVariable("Da
 
 var logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
 
-await new BrowserFetcher().DownloadAsync();
-
-using var browser = await Puppeteer.LaunchAsync(new()
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions()
 {
     Args = new[] { "--no-sandbox" },
 });
 
-try
+await using var context = await browser.NewContextAsync(new()
 {
-    using var page = await browser.NewPageAsync();
+    StorageStatePath = "state.json",
+});
 
-    var response = await page.GoToAsync("http://games.dmm.com/detail/kancolle/");
+var page = await context.NewPageAsync();
 
-    if (response.Url != "https://www.dmm.com/my/-/redirect/=/rurl=DRVESVwZTlVZCFRLHVILWk8GWVsfXQFNAwtbSVkEWVMKDVxc")
-    {
-        logger.Information("Login");
+var response = await page.GotoAsync("http://games.dmm.com/detail/kancolle/", new PageGotoOptions { WaitUntil = WaitUntilState.DOMContentLoaded });
 
-        var loginId = await pg.QuerySingleAsync<string>("SELECT value #>> '{}' FROM store WHERE name = 'login_id';");
-        var loginPassword = await pg.QuerySingleAsync<string>("SELECT value #>> '{}' FROM store WHERE name = 'login_password';");
-
-        await page.WaitForSelectorAsync("#game-play-top", new WaitForSelectorOptions() { Timeout = 0 });
-        await page.ClickAsync("#game-play-top");
-
-        await page.WaitForSelectorAsync("#login_id");
-
-        await page.FocusAsync("#login_id");
-        await page.Keyboard.TypeAsync(loginId, new TypeOptions() { Delay = 50 });
-        //await page.ClickAsync("label[for='save_login_id']");
-        await page.FocusAsync("#password");
-        await page.Keyboard.TypeAsync(loginPassword, new TypeOptions() { Delay = 50 });
-        //await page.ClickAsync("label[for='save_password']");
-        await page.ClickAsync("label[for='use_auto_login']");
-
-        await page.ClickAsync("input[type=submit]", new ClickOptions() { Delay = 1000 });
-        logger.Information("Submit");
-
-        await page.WaitForNavigationAsync();
-    }
-
-    var frameHandle = await page.WaitForSelectorAsync("#game_frame");
-    var frame = await frameHandle.ContentFrameAsync();
-    var gameHandle = await frame.WaitForSelectorAsync("#htmlWrap");
-
-    var url = (await frame.EvaluateExpressionAsync("document.getElementById('htmlWrap').src")).ToString();
-
-    if (url == "http://203.104.209.7/html/maintenance.html")
-    {
-        logger.Information("Maintenance");
-        return;
-    }
-
-    await pg.ExecuteAsync("INSERT INTO store VALUES('game_url', @url::jsonb) ON CONFLICT (name) DO UPDATE SET value = excluded.value;", new { url = '"' + url + '"' });
-
-    logger.Information("Success");
-}
-finally
+if (response.Url is "http://games.dmm.com/detail/kancolle/")
 {
-    await browser.CloseAsync();
+    logger.Information("Login");
+
+    var loginId = await pg.QuerySingleAsync<string>("SELECT value #>> '{}' FROM store WHERE name = 'login_id';");
+    var loginPassword = await pg.QuerySingleAsync<string>("SELECT value #>> '{}' FROM store WHERE name = 'login_password';");
+
+    await page.WaitForSelectorAsync("#game-play-top");
+    await page.ClickAsync("#game-play-top");
+
+    await page.WaitForSelectorAsync("#login_id");
+
+    await page.FocusAsync("#login_id");
+    await page.Keyboard.TypeAsync(loginId, new() { Delay = 50 });
+    await page.ClickAsync("label[for='save_login_id']");
+
+    await page.FocusAsync("#password");
+    await page.Keyboard.TypeAsync(loginPassword, new() { Delay = 50 });
+    await page.ClickAsync("label[for='save_password']");
+
+    await page.ClickAsync("label[for='use_auto_login']");
+    await page.ClickAsync("input[type=submit]");
+    logger.Information("Submit");
+
+    await page.WaitForNavigationAsync();
+
+    await context.StorageStateAsync(new()
+    {
+        Path = "state.json",
+    });
 }
+
+var frameHandle = await page.WaitForSelectorAsync("#game_frame");
+var frame = await frameHandle.ContentFrameAsync();
+var gameHandle = await frame.WaitForSelectorAsync("#htmlWrap");
+
+var url = (await frame.EvaluateAsync("document.getElementById('htmlWrap').src")).ToString();
+
+if (url is "http://203.104.209.7/html/maintenance.html")
+{
+    logger.Information("Maintenance");
+    return;
+}
+
+await pg.ExecuteAsync("INSERT INTO store VALUES('game_url', @url::jsonb) ON CONFLICT (name) DO UPDATE SET value = excluded.value;", new { url = '"' + url + '"' });
+
+logger.Information("Success");
