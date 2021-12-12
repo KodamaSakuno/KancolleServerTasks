476a538c9406d25f59c000d677ce46216dadbfa0
神樹桜乃 <kodama@sakuno.moe>
2021-12-12 16:03:35 +0800

    Fix environment variables

--- DmmLogin/entrypoint.sh
+++ DmmLogin/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo "export $(env | grep DatabaseConn | sed 's/=\(.*\)/="\1"/')" >> .env
+echo "export $(env | grep RedisHost | sed 's/=\(.*\)/="\1"/')" >> .env
+echo "export $(env | grep LoginId | sed 's/=\(.*\)/="\1"/')" >> .env
+echo "export $(env | grep LoginPassword | sed 's/=\(.*\)/="\1"/')" >> .env
 cron start
 tail -f /var/log/cron.log
