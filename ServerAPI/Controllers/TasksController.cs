c1754c27431231ce669e4a3632679eb0f721f87c
神樹桜乃 <kodama@sakuno.moe>
2021-12-18 22:37:44 +0800

    Add task logs api

---
+++ ServerAPI/Controllers/TasksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerAPI.Services;
+
+namespace ServerAPI.Controllers;
+
+[ApiController]
+[Route("tasks")]
+public class TasksController : ControllerBase
+{
+    private readonly TasksService _tasksService;
+
+    public TasksController(TasksService tasksService)
+    {
+        _tasksService = tasksService;
+    }
+
+    [HttpPost("logs:check")]
+    public async Task<string[]> GetLogs() =>
+        await _tasksService.GetLogsAsync();
+}
---
+++ ServerAPI/Services/TasksService.cs
@@ -0,0 +1,34 @@
+﻿using StackExchange.Redis;
+
+namespace ServerAPI.Services;
+
+public sealed class TasksService
+{
+    private readonly IConnectionMultiplexer _redis;
+
+    private LoadedLuaScript? _getLogsScript;
+
+    public TasksService(IConnectionMultiplexer redis)
+    {
+        _redis = redis;
+    }
+
+    public async ValueTask<string[]> GetLogsAsync()
+    {
+        _getLogsScript ??= await EnsureScript();
+
+        var db = _redis.GetDatabase();
+
+        return (string[])await db.ScriptEvaluateAsync(_getLogsScript, new { key = "tasks:logs" });
+
+        Task<LoadedLuaScript> EnsureScript()
+        {
+            var server = _redis.GetServer(_redis.GetEndPoints()[0]);
+            var script = LuaScript.Prepare(@"local result = redis.call('lrange', @key, 0, -1)
+redis.call('del', @key)
+return result");
+
+            return server.ScriptLoadAsync(script);
+        }
+    }
+}
