5062148f5dc6b0c60b6c03344188234ed24b005c
神樹桜乃 <kodama@sakuno.moe>
2021-12-23 23:12:24 +0800

    Fix

--- KcsConstWatcher/entrypoint.sh
+++ KcsConstWatcher/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "export $(env | grep RedisHost | sed 's/=\(.*\)/="\1"/')" >> .env
 echo "export $(env | grep DatabaseConn | sed 's/=\(.*\)/="\1"/')" >> .env
 cron start
 tail -f /var/log/cron.log
