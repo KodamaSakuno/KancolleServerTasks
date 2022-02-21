fac3d1c5e0a6d575924af960e6c732451af0e1a5
神樹桜乃 <kodama@sakuno.moe>
2022-02-21 23:49:55 +0800

    Add android ship cg fetcher project (WIP)

---
+++ AndroidShipCGFetcher/Program.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Serilog;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+using var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+var connectionStringBuilder = new NpgsqlConnectionStringBuilder(configuration["Database"])
+{
+    SearchPath = "kancolle",
+};
+await using var pg = new NpgsqlConnection(connectionStringBuilder.ToString());
+await pg.OpenAsync();
+
+var rabbitMqConnectionFactory = new ConnectionFactory()
+{
+    HostName = configuration["RabbitMQ:Host"],
+    DispatchConsumersAsync = true,
+};
+using var rabbitMqConnection = rabbitMqConnectionFactory.CreateConnection();
+using var rabbitMqChannel = rabbitMqConnection.CreateModel();
+
+const string MasterDataUpdatedExchangeName = "MasterDataUpdated";
+
+rabbitMqChannel.ExchangeDeclare(MasterDataUpdatedExchangeName, ExchangeType.Fanout, true);
+
+var queueName = rabbitMqChannel.QueueDeclare().QueueName;
+rabbitMqChannel.QueueBind(queueName, MasterDataUpdatedExchangeName, string.Empty);
+
+var updatedEventConsumer = new AsyncEventingBasicConsumer(rabbitMqChannel);
+updatedEventConsumer.Received += async (sender, e) =>
+{
+    if (await pg.ExecuteScalarAsync<bool>(""))
+        return;
+
+    await foreach (var item in EnumerateDiffs(pg))
+    {
+        
+    }
+};
+rabbitMqChannel.BasicConsume(queueName, true, updatedEventConsumer);
+
+logger.Information("Waiting...");
+
+await Task.Delay(-1);
+
+static async IAsyncEnumerable<object> EnumerateDiffs(NpgsqlConnection pg)
+{
+    
+}
+    
