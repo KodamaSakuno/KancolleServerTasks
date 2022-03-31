9317c592d27f1b1eeef5e27b9b2195e8b2ac4d01
神樹桜乃 <kodama@sakuno.moe>
2022-03-31 19:57:45 +0800

    Fix

--- DmmLogin/install-browser.sh
+++ DmmLogin/install-browser.sh
@@ -3,7 +3,7 @@
 PACKAGE_ROOT=$(ls -d1 ~/.nuget/packages/microsoft.playwright/* | head -n 1)
 
 cp $PACKAGE_ROOT/buildTransitive/playwright.ps1 .
-cp $PACKAGE_ROOT/lib/net5.0/Microsoft.Playwright.dll .
+cp $PACKAGE_ROOT/lib/netstandard2.0/Microsoft.Playwright.dll .
 cp -r $PACKAGE_ROOT/.playwright .
 
 pwsh playwright.ps1 install chromium
